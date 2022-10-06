8ee10ce490139f43a337de06631e1c6cef6b41d0
ignaarguello <igna.arguello2110@hotmail.com>
2022-10-06 16:30:36 -0300

    Aplicacion de FOR OF a events y correciones - Developer Branch (Commit-2))

--- JS/pastEvents.js
+++ JS/pastEvents.js
@@ -1,29 +1,37 @@
 const ContenedorEvents = document.querySelector('.Contenedor-Cards_Events');
 
-for (let i = 0; i < Data.events.length; i++) {
-    let Card = document.createElement("div");
-    Card.classList += "CardJS";
-  
-    if(Data.currentDate > Data.events[i].date){
 
-        Card.innerHTML = `
-          <div class="max-w-sm rounded overflow-hidden shadow-lg">
-              <img class=" img-card" src="${Data.events[i].image}" alt="Sunset in the mountains">
-          <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2 data-name-card">${Data.events[i].name}</div>
-          <p class="text-gray-700 text-base data-description-card">
-          - ${Data.events[i].description} -
-          </p>
-          </div>
-          <div class="px-6 pt-4 pb-2 data-price-card-button">
-          <h3>${Data.events[i].price}$</h3>
-          <button class='Btn-card'>Read More</button>
-          </div>
-          </div>`;
+function ImprimirCards(array, contenedor){
+    for(let evento of array.events){
+        
+        if(array.currentDate > evento.date){
+            
+            let Card = document.createElement('div');
+            Card.classList += "CardJS";
+            
+            Card.innerHTML = `
+                      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                          <img class=" img-card" src="${evento.image}" alt="Sunset in the mountains">
+                      <div class="px-6 py-4">
+                      <div class="font-bold text-xl mb-2 data-name-card">${evento.name}</div>
+                        <p class="text-gray-700 text-base data-description-card">
+                        - ${evento.description} -
+                        </p>
+                      </div>
+                      <div class="px-6 pt-4 pb-2 data-price-card-button">
+                        <h3>${evento.price}$</h3>
+                        <button class='Btn-card'>Read More</button>
+                      </div>
+                      </div>`
+            
+                 contenedor.appendChild(Card)
+        }
     }
-    else{
-        Card.classList = 'Hidden'
-    }
-  
-    ContenedorEvents.appendChild(Card);
-  }
+}
+
+ImprimirCards(Data, ContenedorEvents)
+
+
+
+
+
