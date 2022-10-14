6589664b34f99324de0759fc06e199a2bfe3c430
ignaarguello <igna.arguello2110@hotmail.com>
2022-10-14 18:58:08 -0300

    Correciones en JS

--- JS/index.js
+++ JS/index.js
@@ -1,26 +1,204 @@
-const ContenedorCards = document.querySelector("#Contenedor-Cards");
+//*Variables que guarda la data
+const DataFinal = Data.events
 
-for (let i = 0; i < Data.events.length; i++) {
-  let Card = document.createElement("div");
-  Card.classList += "CardJS";
-
-  Card.innerHTML = `
-    <div class="max-w-sm rounded overflow-hidden shadow-lg InteriorCard">
-        <img class=" img-card" src="${Data.events[i].image}" alt="Sunset in the mountains">
-    <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2 data-name-card">${Data.events[i].name}</div>
-    <p class="text-gray-700 text-base data-description-card">
-    - ${Data.events[i].description} -
-    </p>
-    <h4 class='CardDate'>Date: ${Data.events[i].date}</h4>
-    </div>
-    <div class="px-6 pt-4 pb-2 data-price-card-button">
-    <h3>${Data.events[i].price}$</h3>
-    <button class='Btn-card'><a href="${Data.events[i].url}">Read More</a></button>
-    </div>
-    </div>`;
-
-  ContenedorCards.appendChild(Card);
+function RenderCards(){
+    let Render = DataFinal.forEach((element)=>{
+    let CardJS = document.createElement('div')
+    CardJS.className = 'CardJS'
+    CardJS.innerHTML += `
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img id="ImagenCard" src="${element.image}" alt="Sunset in the mountains">
+      <div class="px-6 py-4" id='ContenedorBodyCard'>
+      <div class="font-bold text-xl mb-2" id='DataNameCard'>${element.name}</div>
+        <p class="text-gray-700 text-base" id='DataDescriptionCard'>
+          - ${element.description} -
+        </p>
+        <h3 id='CardCategory'>${element.category}</h3>
+        <h4 id='CardDate'>Date: ${element.date}</h4>
+      </div>
+      <div class="px-6 pt-4 pb-2" id='ContenedorDataPrice-ButtonCard'>
+          <h3 id='PriceCard'>${element.price}$</h3>
+          <button id='Btn-card'><a href="${element.url}">Read More</a></button>
+      </div>
+      </div>`
+      ContenedorCardJS.appendChild(CardJS)
+    })
+    return Render
 }
 
+
+//*Variables correspondientes a los checkbox dinamicos
+const ContenedorChecksJS = document.querySelector('#Contenedor-Checks_Index');
+const DataCategory = DataFinal.map(element => element.category)
+const DataCategorySet = new Set([...DataCategory])
+
+
+//?Render de los checkbox dinamicos
+DataCategorySet.forEach((element)=>{
+    let DivChecksJS = document.createElement('div')
+    DivChecksJS.id = 'DivJS'
+    
+    DivChecksJS.innerHTML += `
+
+    <label id='LabelJS' for="${element}">${element}</label>
+    <input class='InputCheck${element}' type="checkbox" value="${element}">
+  
+    `
+
+    ContenedorChecksJS.appendChild(DivChecksJS)
+})
+
+
+//*Variable y Render de las Card en Index
+const ContenedorCardJS = document.querySelector('#Contenedor-Card-Index');
+
+DataFinal.forEach((element)=>{
+  
+  let CardJS = document.createElement('div')
+  CardJS.className = 'CardJS'
+
+    CardJS.innerHTML += `
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <img id="ImagenCard" src="${element.image}" alt="Sunset in the mountains">
+    <div class="px-6 py-4" id='ContenedorBodyCard'>
+    <div class="font-bold text-xl mb-2" id='DataNameCard'>${element.name}</div>
+      <p class="text-gray-700 text-base" id='DataDescriptionCard'>
+        - ${element.description} -
+      </p>
+      <h3 id='CardCategory'>${element.category}</h3>
+      <h4 id='CardDate'>Date: ${element.date}</h4>
+    </div>
+    <div class="px-6 pt-4 pb-2" id='ContenedorDataPrice-ButtonCard'>
+        <h3 id='PriceCard'>${element.price}$</h3>
+        <button id='Btn-card'><a href="${element.url}">Read More</a></button>
+    </div>
+    </div>`
+
+    ContenedorCardJS.appendChild(CardJS)
+  })
+
+
+  //* Variables donde seleccionamos los checkbox dinamicos por query
+  const CheckFoodFair = document.querySelector('.InputCheckFoodFair')
+  const CheckMuseum = document.querySelector('.InputCheckMuseum')
+  const CheckCostumeParty = document.querySelector('.InputCheckCostumeParty')
+  const CheckMusicConcert = document.querySelector('.InputCheckMusicConcert')
+  const CheckRace = document.querySelector('.InputCheckRace')
+  const CheckBookExchange = document.querySelector('.InputCheckBookExchange')
+  const CheckCinema = document.querySelector('.InputCheckCinema')
+
+  
+  let eventosFiltrados = []
+
+  function Filter(value){
+      //Filtra segun el evento
+      let dataFilter = DataFinal.filter(element => element.category.includes(value))
+      
+      eventosFiltrados.push(dataFilter)  
+
+      eventosFiltrados.forEach(element => element.forEach((element)=>{
+        let CardJS = document.createElement('div')
+        CardJS.className = 'CardJS'
+        
+        CardJS.innerHTML += `
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+          <img id="ImagenCard" src="${element.image}" alt="Sunset in the mountains">
+        <div class="px-6 py-4" id='ContenedorBodyCard'>
+        <div class="font-bold text-xl mb-2" id='DataNameCard'>${element.name}</div>
+          <p class="text-gray-700 text-base" id='DataDescriptionCard'>
+            - ${element.description} -
+          </p>
+          <h3 id='CardCategory'>${element.category}</h3>
+          <h4 id='CardDate'>Date: ${element.date}</h4>
+        </div>
+        <div class="px-6 pt-4 pb-2" id='ContenedorDataPrice-ButtonCard'>
+          <h3 id='PriceCard'>${element.price}$</h3>
+          <button id='Btn-card'><a href="${element.url}">Read More</a></button>
+        </div>
+      </div>`
+      ContenedorCardJS.appendChild(CardJS)
+      }))
+
+      console.log(eventosFiltrados)
+      
+      return eventosFiltrados
+  }
+
+
+//*Evento Checkbox Foor Fair
+  CheckFoodFair.addEventListener('change', (event) => {
+    if(CheckFoodFair.checked){
+        ContenedorCardJS.innerHTML = ``
+        Filter(event.target.value)
+      } 
+      else{
+        RenderCards()
+      }
+  })
+
+  //*Evento Checkbox Museum
+  CheckMuseum.addEventListener('change', (event) => {
+    if(CheckMuseum.checked){
+        ContenedorCardJS.innerHTML = ``
+        Filter(event.target.value)
+      }
+      else{
+        RenderCards()
+      }
+  })
+
+  //*Evento Checkbox Costume Party
+  CheckCostumeParty.addEventListener('change', (event) => {
+    if(CheckCostumeParty.checked){
+        ContenedorCardJS.innerHTML = ``
+        Filter(event.target.value)
+      }
+      else{
+        RenderCards()
+      }
+  })
+
+  //*Evento Checkbox Music Concert
+  CheckMusicConcert.addEventListener('change', (event) => {
+    if(CheckMusicConcert.checked){
+        ContenedorCardJS.innerHTML = ``
+        Filter(event.target.value)
+      }
+      else{
+        RenderCards()
+      }
+  })
+
+  //*Evento Checkbox Race
+  CheckRace.addEventListener('change', (event) => {
+    if(CheckRace.checked){
+        ContenedorCardJS.innerHTML = ``
+        Filter(event.target.value)
+      }
+      else{
+        RenderCards()
+      }
+  })
+
+  //*Evento Checkbox Book Exchange
+  CheckBookExchange.addEventListener('change', (event) => {
+    if(CheckBookExchange.checked){
+        ContenedorCardJS.innerHTML = ``
+        Filter(event.target.value)
+      }
+      else{
+        RenderCards()
+      }
+  })
+
+  //*Evento Checkbox Cinema
+  CheckCinema.addEventListener('change', (event) => {
+    if(CheckCinema.checked){
+        ContenedorCardJS.innerHTML = ``
+        Filter(event.target.value)
+      }
+      else{
+        RenderCards()
+      }
+  })
 
--- JS/pastEvents.js
+++ JS/pastEvents.js
@@ -1,36 +1,41 @@
-const ContenedorEvents = document.querySelector('.Contenedor-Cards_Events');
+const ContenedorPastEvents = document.querySelector('.Contenedor-Cards_Events');
 
 
-function ImprimirCards(array, contenedor){
-    for(let evento of array.events){
-        
-        if(array.currentDate > evento.date){
-            
-            let Card = document.createElement('div');
-            Card.classList += "CardJS";
-            
-            Card.innerHTML = `
-                      <div class="max-w-sm rounded overflow-hidden shadow-lg">
-                          <img class=" img-card" src="${evento.image}" alt="Sunset in the mountains">
-                      <div class="px-6 py-4">
-                      <div class="font-bold text-xl mb-2 data-name-card">${evento.name}</div>
-                        <p class="text-gray-700 text-base data-description-card">
-                        - ${evento.description} -
-                        </p>
-                        <h4 class='CardDate'>Date: ${evento.date}</h4>
-                      </div>
-                      <div class="px-6 pt-4 pb-2 data-price-card-button">
-                        <h3>${evento.price}$</h3>
-                        <button class='Btn-card'><a href="${evento.url}">Read More</a></button>
-                      </div>
-                      </div>`
-            
-                 contenedor.appendChild(Card)
-        }
-    }
-}
 
-ImprimirCards(Data, ContenedorEvents)
+
+
+
+
+
+
+/* Data.events.forEach(evento => {
+    let card = document.createElement('div');
+    card.classList += "CardJS";
+    card.innerHTML += `
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class=" img-card" src="${evento.image}" alt="Sunset in the mountains">
+      <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2 data-name-card">${evento.name}</div>
+        <p class="text-gray-700 text-base data-description-card">
+          - ${evento.description} -
+        </p>
+        <h4 class='CardDate'>Date: ${evento.date}</h4>
+        </div>
+        <div class="px-6 pt-4 pb-2 data-price-card-button">
+          <h3>${evento.price}$</h3>
+          <button class='Btn-card'><a href="${evento.url}">Read More</a></button>
+        </div>
+        </div>`
+
+        ContenedorPastEvents.appendChild(card)
+}); */
+
+
+
+
+
+
+
 
 
 
